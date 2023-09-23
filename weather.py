4789286e98b3e31067e82c65040801100323a408
luufqa <luufqa@gmail.com>
2023-09-23 18:23:38 +0400

    Fixing code and visual component

--- weather.py
+++ weather.py
@@ -3,154 +3,124 @@ import requests
 from typing import List, Tuple
 
 # url api с параметрами
-urlWeather = "https://api.open-meteo.com/v1/forecast?"
-urlGeocoding = "https://geocoding-api.open-meteo.com/v1/search?name="
+URL_WEATHER = "https://api.open-meteo.com/v1/forecast?"
+URL_GEOCODING = "https://geocoding-api.open-meteo.com/v1/search?name="
 
-yesSeriesDays = 0
-startDate = ""         #dt.today()
-endDate = startDate            #startDate
-lat = 0
-lon = 0
-dataTemp = ""
-tempCity = ""
-pastDay = 0
-suns = ""
-sunr = ""
 
-def stAndDat(dateR: str) -> Tuple[str, str]:
-    """Function return default start-end date or user specified start-end date
-    :param dateR: Start index, in format "YYYY-MM-DD"
-    :type str: date, optional
-    :return: response with diapason start-end date
-    :rtype: str
-    """
-    s = dt.now().strftime("%Y-%M-%D")
-    if dateR == s:
-        pass
-    else:
-        global startDate, endDate
-        startDate = dateR
-        endDate = startDate
-    return startDate, endDate
-
-def sunsetSunrise(suns: str, sunr: str) -> Tuple[str, str]:
-    """Function return date suns and sunr in city by coords. Put time in result.
-    :param suns: First index, sunset date in list
-    :type str:
-    :param sunr: Second index, sunrise date in list
-    :type str:
-    :return: Put time in result
-    :rtype: str
-    """
-    for a in suns:
-        print(f"Sunset in you city in - {a[-5:]}")
-    for b in sunr:
-        print(f"Sunrise in you city in - {b[-5:]}")
-    return a, b
-
-def get_cityName(city: str) -> str:
+def get_city_name(city: str) -> str:
     """This function return city name.
     :param city: The name to use
-    :type str: str
     :return: city name
-    :rtype: str
     """
     return city
 
-def get_coords_by_yourCity(youCity: str) -> Tuple[float, float]:
-    """This function return coords by city.
-    :param youCity: The name to use
-    :type str: str
-    :return: coords lat and lon
-    :rtype: str
+
+def get_sunset_sunrise(suns: str, sunr: str) -> Tuple[str, str]:
+    """Function return date suns and sunr in city by coords. Put time in result.
+    :param suns: First index, sunset date in list
+    :param sunr: Second index, sunrise date in list
+    :return: Put time in result
     """
-    Geocoding = requests.get(urlGeocoding + youCity).json()
-    global lat, lon
+
+    for a in suns:
+        a = f"Sunset in you city in - {a[-5:]}"
+        for b in sunr:
+            b: str = f"Sunrise in you city in - {b[-5:]}"
+            return a, b
+
+
+def get_coords_by_your_city(youcity: str) -> Tuple[float, float]:
+    """This function return coords by city.
+    :param youcity: The name to use
+    :return: coords lat and lon
+    """
+
+    Geocoding = requests.get(URL_GEOCODING + youcity).json()
+
     lat = Geocoding["results"][0]["latitude"]
     lon = Geocoding["results"][0]["longitude"]
     return lat, lon
 
-def get_weather_by_coords(lat: float, lon: float, yesSeriesDays=1, pastDay=0) -> List[str]:
+
+def get_weather_by_coords(lat: float, lon: float, next_days=1, past_days=0) -> List[str]:
     """This function return weather by coords. Default weather for today (first day)
     :param lat: coords city - latitude
-    :type float: float
     :param lon: coords city - longitude
-    :type float: float
-    :param yesSeriesDays: how many days
-    :type int: int
-    :param pastDay: previous days
-    :type int: int
+    :param next_days: how many days
+    :param past_days: previous days
     :return: return weather by coords
-    :rtype: str
     """
-    weatherYourCity = urlWeather + f"latitude={lat}" + "&" + f"longitude={lon}" + "&hourly=temperature_2m" + f"&forecast_days={yesSeriesDays}" + f"&past_days={pastDay}"
+    weatherYourCity = URL_WEATHER + f"latitude={lat}" + "&" + f"longitude={lon}" + "&hourly=temperature_2m" \
+                      + f"&forecast_days={next_days}" + f"&past_days={past_days}"
     reqWeatherYourCity = requests.get(weatherYourCity).json()
 
-    global dataTemp, tempCity
     dataTemp = reqWeatherYourCity["hourly"]["time"]
     tempCity = reqWeatherYourCity["hourly"]["temperature_2m"]
-    # формируем красивый вывод погоды на сегодня
+
     compilationDataTemp = []
-    # def build(dataTemp: str, tempCity: str) -> List[str]:
     for d, t in zip(dataTemp, tempCity):
         res = (str(d) + " - " + str(t) + " °C")
         compilationDataTemp.append(res)
 
     return compilationDataTemp
 
-def last(date: str, lat: float, lon: float, yesSeriesDays=0) -> Tuple[str, str]:
+
+def sunset_sunrise_by_city(youcity: str, date: str, next_days=0) -> Tuple[str, str]:
     """This function return value sunset and sunrise. Default sunset/sunrise for today (first day)
-    :param date: Start index, in format "YYYY-MM-DD"
-    :type str: str
-    :param lat: coords city - latitude
-    :type float: float
-    :param lon: coords city - longitude
-    :type float: float
-    :param yesSeriesDays: how many days
-    :type int: int
+    :param youcity: Start index, enter your city name in str "city"
+    :param date: Second index, in format "YYYY-MM-DD"
+    :param next_days: how many days
     :return: return weather by coords
-    :rtype: Tuple[str,str]
     """
-    stAndDat(date)
-    weatherYourCity = urlWeather + f"latitude={lat}" + "&" + f"longitude={lon}" + f"&forecast_days={yesSeriesDays}" + f"&daily=sunset,sunrise" + f"&timezone=auto" + f"&start_date={startDate}" + f"&end_date={endDate}"
+    Geocoding = requests.get(URL_GEOCODING + youcity).json()
+    lat = Geocoding["results"][0]["latitude"]
+    lon = Geocoding["results"][0]["longitude"]
+    startDate = ""
+    endDate = ""
+    s = dt.now().strftime("%Y-%M-%D")
+    if date == s:
+        pass
+    else:
+        startDate = date
+        endDate = startDate
+
+    weatherYourCity = URL_WEATHER + f"latitude={lat}" + "&" + f"longitude={lon}" + f"&forecast_days={next_days}" \
+                      + f"&daily=sunset,sunrise" + f"&timezone=auto" + f"&start_date={startDate}" \
+                      + f"&end_date={endDate}"
     reqWeatherYourCity = requests.get(weatherYourCity).json()
 
-    global suns, sunr
     suns = reqWeatherYourCity["daily"]["sunset"]
     sunr = reqWeatherYourCity["daily"]["sunrise"]
-    return sunsetSunrise(suns, sunr)
+    return get_sunset_sunrise(suns, sunr)
 
-def get_weather_by_cityName(weather, coords):
+
+def get_weather_by_city_name(youcity: str, next_days=0):
     """Function return weather on the next two days or another by your city name
-    :param weather: Start index, give func "get_weather_by_coords" and func "get_coords_by_yourCity"
-    :type funcs:
+    :param youcity: Start index, give city name in str
+    :param next_days: Second index, give number for next day
     :return: List with weather on the two days or another by your city name
-    :rtype: Tuple[Any, Any]
     """
-    return weather, coords
+    Geocoding = requests.get(URL_GEOCODING + youcity).json()
+    lat = Geocoding["results"][0]["latitude"]
+    lon = Geocoding["results"][0]["longitude"]
+    return get_weather_by_coords(lat, lon, next_days)
 
-def get_weatherPastday_by_cityName(coords, weather):
-    """Function return weather on the one pastday or another pastday, by your city name
-    :param weather: Start index, give func "get_weather_by_coords" and func "get_coords_by_yourCity"
-    :type funcs:
+
+def get_weather_past_days_by_city_name(youcity: str, past_days=0):
+    """Function return weather on the one past_days or another past_days, by your city name
+    :param youcity: Start index, give city name in str
+    :param past_days: Second index, give number for next past day
     :return: List with weather on the two days or another by your city name
-    :rtype: Tuple[Any, Any]
     """
-    return coords, weather
 
-def get_sunsetSunrise_by_city(coords, sunsetAndSunrise):
-    """Function return time when sunset and sunrise for the day, in the city that we specify
-    :param weather: Start index, give func "get_weather_by_coords" and func "last"
-    :type funcs:
-    :return: List with weather on the two days or another by your city name
-    :rtype: Tuple[Any, Any]
-    """
-    return coords, sunsetAndSunrise
+    Geocoding = requests.get(URL_GEOCODING + youcity).json()
+    lat = Geocoding["results"][0]["latitude"]
+    lon = Geocoding["results"][0]["longitude"]
+    return get_weather_by_coords(lat, lon, past_days)
 
-
-#print(get_cityName("Moscow"))
-#print(get_coords_by_yourCity(get_cityName("Moscow")))
-#print(get_weather_by_coords(55.75222, 37.61556, yesSeriesDays=2))
-#print(get_weather_by_cityName(get_weather_by_coords(lat, lon, yesSeriesDays=2), get_coords_by_yourCity(get_cityName("Moscow"))))
-#print(get_weatherPastday_by_cityName(get_weather_by_coords(lat, lon, yesSeriesDays, pastDay=1), get_coords_by_yourCity(get_cityName("Moscow"))))
-#get_sunsetSunrise_by_city(get_coords_by_yourCity(get_cityName("Berlin")), last("2023-09-30", lat, lon))
+# print(get_city_name("Moscow"))
+# print(get_coords_by_your_city(get_city_name("Berlin")))
+# print(get_weather_by_coords(55.75222, 37.61556, next_days=2))
+# print(get_weather_by_city_name("Berlin", next_days=2))
+# print(get_weather_past_days_by_city_name("Moscow", past_days=2))
+# print(sunset_sunrise_by_city("newyork", "2023-09-30"))
